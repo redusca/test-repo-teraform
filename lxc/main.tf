598ea20f0a74fb14dc02ffef782ef86e1abd5f1e
redusca <alwxvidami@gmail.com>
2025-09-26 14:52:08 +0300

    AAAA

--- lxc/main.tf
+++ lxc/main.tf
@@ -2,6 +2,7 @@
 # Copyright: 2025-2030 Denis Rendler
 # Repository: https://github.com/rendler-denis/tf-proxmox-mod
 # License: Check the LICENSE file or the repository for the license of the module.
+# EDIT COMMENT
 
 terraform {
   required_version = ">= 0.0.1"
